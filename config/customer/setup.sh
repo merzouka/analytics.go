0d401fb63edaf8d08ef1ec34af2e288cfcb3a5dc
merzouka <marzoukayouness@gmail.com>
2024-08-08 15:47:52 +0100

    Changes: - fixed customer service code - fixed up config

--- config/customer/setup.sh
+++ config/customer/setup.sh
@@ -8,13 +8,14 @@ get_service_url () {
     echo "$1.customer.svc.cluster.local"
 }
 
-db_service="db-service"
+db_service="database"
+db_name="customerdb"
 db_password=$(echo -n $(cat .db))
-db_url="postgres://docker:$db_password/$db_service.customer.svc.cluster.local:5432/customerdb"
-kubectl create secret generic db-secret --from-literal=db-password=$db_password --from-literal=db-url=$db_url
+db_url="postgres://docker:$db_password/$db_service.customer.svc.cluster.local:5432/$db_name"
+kubectl create secret generic db-secret --from-literal=db-password=$db_password
 
 kubectl create secret generic customer-secrets \
-    --from-literal=db-url="postgresql://docker:$db_password@$(get_service_url "database"):5432" \
+    --from-literal=db-url="postgresql://docker:$db_password@$(get_service_url "$db_service"):5432/$db_name" \
     --from-literal=cache-url="$(get_service_url "cache"):6379" \
     --from-literal=cache-password=$(echo -n $(cat .cache))
 
