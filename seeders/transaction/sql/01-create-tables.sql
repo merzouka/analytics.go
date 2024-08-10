687921db57f2d0568907d69af34ee5e7c4063bf5
merzouka <marzoukayouness@gmail.com>
2024-08-10 18:47:30 +0100

    Changes: - added seeder logic for transactions - finished transaction logic Next: - change customers and move customer-transaction association to transaction

--- seeders/transaction/sql/01-create-tables.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE IF NOT EXISTS transactions (id SERIAL PRIMARY KEY, client_id BIGINT, created_at TIMESTAMP WITH TIME ZONE);
-CREATE TABLE IF NOT EXISTS transaction_products (transaction_id BIGINT, product_id BIGINT);
